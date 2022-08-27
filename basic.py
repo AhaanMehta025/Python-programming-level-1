886c5365ca53c0c70256773125a6278dd4cb0c6c
Ahaan <ahaan.mehta@gmail.com>
2022-08-27 15:37:42 +0530

    commiting files

--- basic.py
+++ basic.py
@@ -27,8 +27,38 @@
 #     j+=1
 
 #--------------module_-----------------
-import math
+# import math
 
 
 # from random import randint
 # print(randint(1,10))
+
+#-------------dictionary------------
+# a = 'Ths is a sentence.'
+# a = ['This is sentence 1','This is sentence 2']
+# if 'This is sentence 1' in a:
+#     print('yes')
+
+d = {
+    'name': 'ahaan',
+    'is':'okay',
+    'type':{'1':'sad','2':'happy'}
+
+
+}
+for a,d in d['type'].items():
+    print(a,'is',d)
+# # print(d['type'][1])
+
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+
+# for a,d in d.items():
+#     print(a,'is',d)
+
+ #----------string sclicing----------------
+
+# a='this'
+
+# print(a[1:3])
